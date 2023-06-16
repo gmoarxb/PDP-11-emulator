004bb3f19f4fcd93a3a463ec323abe66f373812d
Power Brandie <powerbra@student.21-school.ru>
2023-06-16 13:59:25 +0300

    added a commid line option (data path arg)

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdlib.h>
+#include <getopt.h>
 #include "pdp.h"
 
-int main() {
+#define COMMAND_OPTIONS ""
+
+int main(int argc, char* argv[]) {
+    if (argc <= 1) {
+        error("main", "Need a path to data in command line!");
+    }
+    while (getopt(argc, argv, COMMAND_OPTIONS) != -1) {
+    }
+    char* data_path = argv[argc - 1];
     Memory mem = {0};
-    memory_load_data(&mem, "./test/data.txt");
+    memory_load_data(&mem, data_path);
     memory_dump(&mem, 0x0200, 0x000c);
     memory_dump(&mem, 0x0040, 0x000c);
     return EXIT_SUCCESS;
